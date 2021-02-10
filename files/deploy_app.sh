47337d2867280d0ae28f7e412be573e58163dd6e
Zeff Morgan <zeff.morgan@gmail.com>
2021-02-10 20:15:39 +0000

    Add API changes

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -4,14 +4,14 @@
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Meow!</title></head>
+  <head><title>Bill!</title></head>
   <body>
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
-  ${PREFIX} invites you to the wonderful world of kittens!
+  <center><h2>Murray World!</h2></center>
+  ${PREFIX} invites you to the wonderful world of Bill Murray!
   <!-- END -->
 
   </div>
