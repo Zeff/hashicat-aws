324bdb15ee79581aead8d9cd70039e01f9c79f51
Zeff Morgan <zeff.morgan@gmail.com>
2021-02-10 20:56:13 +0000

    Update vars and deploy

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,7 +9,7 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Murray World!</h2></center>
   ${PREFIX} invites you to the wonderful world of Bill Murray!
   <!-- END -->
